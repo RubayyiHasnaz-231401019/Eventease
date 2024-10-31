44e6999dec6d9f20f8d957f5c71700cbe99a8e68
NyssaObelia-231401097 <153521459+NyssaObelia-231401097@users.noreply.github.com>
2024-10-31 13:44:42 +0700

    Update profile-email.js

--- js/profile-email.js
+++ js/profile-email.js
@@ -1,9 +1,9 @@
 document.querySelector('.save-button button').addEventListener('click', function() {
-    // Mengambil nilai dari input
+    
     const newEmail = document.getElementById('first-name').value.trim();
     const confirmEmail = document.getElementById('last-name').value.trim();
 
-    // Memeriksa apakah semua input terisi
+ 
     if (newEmail === '' || confirmEmail === '') {
         alert('Please fill in all fields!');
     } else if (newEmail !== confirmEmail) {
