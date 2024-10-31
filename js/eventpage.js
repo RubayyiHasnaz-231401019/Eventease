42da44a8f4dd8623bad23c18903bfcb088ac25b7
NyssaObelia-231401097 <153521459+NyssaObelia-231401097@users.noreply.github.com>
2024-10-31 13:41:56 +0700

    Update eventpage.js

--- js/eventpage.js
+++ js/eventpage.js
@@ -1,31 +1,31 @@
 const followButton = document.querySelector('.follow');
 
-// Cek status dari localStorage saat halaman dimuat
+
 if (localStorage.getItem('followStatus') === 'unfollow') {
     followButton.classList.add('unfollow');
     followButton.classList.remove('follow');
     followButton.textContent = 'Unfollow';
 }
 
-// Tambahkan event listener untuk mengubah status
+
 followButton.addEventListener('click', function () {
     if (followButton.classList.contains('follow')) {
         followButton.classList.remove('follow');
         followButton.classList.add('unfollow');
         followButton.textContent = 'Unfollow';
-        localStorage.setItem('followStatus', 'unfollow'); // Simpan status di localStorage
+        localStorage.setItem('followStatus', 'unfollow');
     } else {
         followButton.classList.remove('unfollow');
         followButton.classList.add('follow');
         followButton.textContent = '+ Follow';
-        localStorage.setItem('followStatus', 'follow'); // Simpan status di localStorage
+        localStorage.setItem('followStatus', 'follow');
     }
 });
 
-let isAvailable = false; // Status awal tiket
+let isAvailable = false;
 
     function toggleAvailability() {
-        isAvailable = !isAvailable; // Toggle status tiket
+        isAvailable = !isAvailable;
 
         const availabilityDiv = document.getElementById('ticketAvailability');
         
